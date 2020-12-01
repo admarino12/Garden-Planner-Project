af3c3052810703b9bd00f722795f67ac7ec3321d
cegraham <cegraham@udel.edu>
2020-12-01 13:39:50 -0500

    front page view and start btn implemented

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,13 +1,24 @@
 package src.main.java;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class View {
@@ -32,6 +43,44 @@ public class View {
 	public View(Stage theStage, Controller control) {
 		this.control = control;
 		this.theStage = theStage;
+		
+		//Front Page View
+		Stage popupwindow = new Stage();
+	      
+		popupwindow.initModality(Modality.APPLICATION_MODAL);
+		popupwindow.setTitle("Front Page");
+		      
+		//start button to get to garden view
+		Button startBtn = new Button("Start New Garden"); 
+		startBtn.setMaxSize(200, 200);
+		startBtn.setStyle("-fx-text-alignment: center");
+		startBtn.setStyle("-fx-background-color: #D1EBDC");
+		startBtn.setOnAction(e -> popupwindow.close()); //close pop up on click
+		VBox layout = new VBox(10);
+		
+		//import background image
+		Image image;
+		try {
+			image = new Image(new FileInputStream("src/resources/images/Front_Page.png"));
+			BackgroundImage bg = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, null, null);
+			Background background = new Background(bg);
+			layout.setBackground(background);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		
+		//setting up layout of Front Page
+		layout.setPadding(new Insets(220,10,10,10));
+		layout.getChildren().addAll(startBtn);
+		layout.setAlignment(Pos.CENTER);
+		
+		//set Front Page scene
+		Scene scene1= new Scene(layout, 1225, 760); 
+		popupwindow.setScene(scene1);
+		popupwindow.showAndWait(); //once pop up is closed, Garden Builder View is shown
+		
+		
+		//Garden Builder View
 		theStage.setTitle("Garden Builder");
 		Group root = new Group();
 		Scene theScene = new Scene(root, ROOT_WIDTH, getROOT_HEIGHT());
@@ -90,9 +139,6 @@ public class View {
     	control.setHandlerForRemoveClick(imgView);
     	control.setHandlerForPlantDragged(imgView);
  
-  
-    	
-    	
     	border.getChildren().add(imgView);
 	}
 	
